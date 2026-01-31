e1bd2e612a956fcd3feb4285280ed8dde5c588b5
SimplyMinimal <SimplyMinimal@users.noreply.github.com>
2026-01-31 12:30:49 -0500

    English localized text

--- Maccy/Views/PreviewItemView.swift
+++ Maccy/Views/PreviewItemView.swift
@@ -26,7 +26,7 @@ struct PreviewItemView: View {
 
         if let application = item.application {
           HStack(spacing: 3) {
-            Text("Application", tableName: "PreviewItemView")
+            Text(NSLocalizedString("application", comment: ""))
             Image(nsImage: item.applicationImage.nsImage)
               .resizable()
               .frame(width: 11, height: 11)
@@ -35,40 +35,40 @@ struct PreviewItemView: View {
         }
 
         HStack(spacing: 3) {
-          Text("FirstCopyTime", tableName: "PreviewItemView")
+          Text(NSLocalizedString("first_copy_time", comment: ""))
           Text(item.item.firstCopiedAt, style: .date)
           Text(item.item.firstCopiedAt, style: .time)
         }
 
         HStack(spacing: 3) {
-          Text("LastCopyTime", tableName: "PreviewItemView")
+          Text(NSLocalizedString("last_copy_time", comment: ""))
           Text(item.item.lastCopiedAt, style: .date)
           Text(item.item.lastCopiedAt, style: .time)
         }
 
         HStack(spacing: 3) {
-          Text("NumberOfCopies", tableName: "PreviewItemView")
+          Text(NSLocalizedString("number_of_copies", comment: ""))
           Text(String(item.item.numberOfCopies))
         }
         .padding(.bottom)
 
         if let pinKey = KeyboardShortcuts.Shortcut(name: .pin) {
           Text(
-            NSLocalizedString("PinKey", tableName: "PreviewItemView", comment: "")
+            NSLocalizedString("pin_key", comment: "")
               .replacingOccurrences(of: "{pinKey}", with: pinKey.description)
           )
         }
 
         if let deleteKey = KeyboardShortcuts.Shortcut(name: .delete) {
           Text(
-            NSLocalizedString("DeleteKey", tableName: "PreviewItemView", comment: "")
+            NSLocalizedString("delete_key", comment: "")
               .replacingOccurrences(of: "{deleteKey}", with: deleteKey.description)
           )
         }
 
         if item.item.image != nil, let copyImageTextKey = KeyboardShortcuts.Shortcut(name: .copyImageText) {
           Text(
-            NSLocalizedString("CopyImageTextKey", tableName: "PreviewItemView", comment: "")
+            NSLocalizedString("copy_image_text_key", comment: "")
               .replacingOccurrences(of: "{copyImageTextKey}", with: copyImageTextKey.description)
           )
         }
